933b54f35797a06a8162a0fa1a6b0609829e81f4
iliussagar <sagar01830@gmail.com>
2023-10-27 11:45:18 +0600

    vite latest install

---
+++ resources/views/Note.blade.php
@@ -0,0 +1,7 @@
+composer create-project laravel/laravel example-app
+------------------------------------------------------------
+npm create vite@latest
+cd project
+npm install
+npm run dev
+---------------------------------------------------------
