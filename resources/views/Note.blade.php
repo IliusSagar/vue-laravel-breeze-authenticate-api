1309c2222a6b8da4facef4eebb3fc67f214f0826
iliussagar <sagar01830@gmail.com>
2023-10-27 12:40:18 +0600

    env backend Domain setup

--- resources/views/Note.blade.php
+++ resources/views/Note.blade.php
@@ -5,3 +5,14 @@ cd project
 npm install
 npm run dev
 ---------------------------------------------------------
+App.vue**
+-----------------------------------------
+composer require laravel/breeze --dev
+composer require laravel/breeze:1.9.4
+php artisan breeze:install api
+.env change URL
+-------------------------------------------
+.env**
+-------------------------------------
+php artisan migrate
+-----------------------------------------------
